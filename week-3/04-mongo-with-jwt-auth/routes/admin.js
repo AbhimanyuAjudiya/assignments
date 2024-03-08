c52de196745d02e35b5840fa219c1b79374ae192
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2024-03-09 03:10:58 +0530

    added solution of jwt auth in week3

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,72 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const { Admin, Course } = require("../db");
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require("../config");
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await Admin.create({
+        username : username,
+        password : password
+    }).then(function(val){
+        res.status(200).send("Admin created successfully");
+    }).catch(function(err){
+        res.status(400).send("Admin already exists");
+    })
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password
+
+    const user = await Admin.findOne({
+        username : username,
+        password : password
+    })
+    if(user){
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+        res.status(200).send({token});
+    } else {
+        res.status(400).send("Invalid username or password");
+    }
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+
+    Course.create({
+        title,
+        description,
+        imageLink,
+        price
+    }).then(function(val){
+        res.json({
+            msg:"Course created successfully",
+            courseId: val._id
+        });
+    }).catch(function(err){
+        res.status(400).send("Course already exists");
+    })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    Course.find({}).then(function(val){
+        res.json(val);
+    })
 });
 
 module.exports = router;
